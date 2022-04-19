2ce68ee006986f466bd1bbbd856ccbc2dc6743ee
sultanragil <71801903+sultanragil@users.noreply.github.com>
2022-04-19 10:49:12 +0700

    Add Template + Dashboard + Migrations

--- website_profile_fix/routes/web.php
+++ website_profile_fix/routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['namespace'=>'Backend'], function () {
+    Route::prefix('dashboard')->group(function () {
+        Route::resource('/', '\App\Http\Controllers\Backend\IndexController');
+        
+    });
+});
