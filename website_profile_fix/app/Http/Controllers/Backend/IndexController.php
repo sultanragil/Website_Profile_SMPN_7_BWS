a4b1977a4212489bcd4040ec4d8280c087cbe2b6
AzizRahmawan <arder058@gmail.com>
2022-04-19 23:48:56 +0700

    Auth Login

--- website_profile_fix/app/Http/Controllers/Backend/IndexController.php
+++ website_profile_fix/app/Http/Controllers/Backend/IndexController.php
@@ -9,6 +9,10 @@ use Illuminate\Support\Facades\DB;
 
 class IndexController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('admin'); /* Cara agar cuman super admin*/
+    }
     public function index()
     {
         return view('backend.index');
