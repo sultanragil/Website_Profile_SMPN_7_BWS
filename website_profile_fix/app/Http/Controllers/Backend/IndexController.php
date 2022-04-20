6ee6dd3cc1e5f44d0b7cb89b3b3d05849ecc892a
AzizRahmawan <arder058@gmail.com>
2022-04-20 08:26:25 +0700

    dash auth off

--- website_profile_fix/app/Http/Controllers/Backend/IndexController.php
+++ website_profile_fix/app/Http/Controllers/Backend/IndexController.php
@@ -11,7 +11,8 @@ class IndexController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('admin'); /* Cara agar cuman super admin*/
+        /*$this->middleware('auth'); /*Hak Akses Login*/
+        /*$this->middleware('admin'); /*Hak Akses Super Admin*/
     }
     public function index()
     {
